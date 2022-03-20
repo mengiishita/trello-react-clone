50a9f095928b2e17fa93de7e9224ed3769b15c40
“mengiishita” <mengiishita@gmail.com>
2022-03-20 11:43:41 +0100

    Board and List component - setup

---
+++ src/components/Layout/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from '../List/List';
+
+const Board = () => {
+	return (
+		<div>
+            Project Trello
+			<List />
+		</div>
+	);
+};
+
+export default Board;
---
+++ src/components/List/List.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const List = () => {
+    return <div>
+        <p>Todo</p>
+    </div>
+}
+
+export default List;
